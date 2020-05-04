cbb935fda8aef017aa6c6aef5375d423bd426b0d
Ross McLean <rmclean92@protonmail.com>
2020-05-04 14:48:26 +0100

    Relax User Password Requirements
    - set required character length to 1
    - no longer require non letter or digit
    - no longer require digit
    - no longer require lowercase
    - no longer require uppercase

--- CycletexBikesMvc/App_Start/IdentityConfig.cs
+++ CycletexBikesMvc/App_Start/IdentityConfig.cs
@@ -53,11 +53,12 @@ namespace CycletexBikesMvc
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                // Changes RequiredLength for sanities sake while debugging.
+                RequiredLength = 1,  // Default value is 6.
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
