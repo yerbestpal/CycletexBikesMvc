739d3d6f0230f80aec499f36dea23e3898f1f6ea
Ross McLean <rmclean92@protonmail.com>
2020-05-04 15:35:52 +0100

    Implement Staff class.

---
+++ CycletexBikesMvc/Models/Staff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CycletexBikesMvc.Models
+{
+    /// <summary>
+    /// Main Staff class.
+    /// Sets properties for all Staff types.
+    /// Extends ApplicationUser class.
+    /// </summary>
+    public class Staff : ApplicationUser
+    {
+        public string Address { get; set; }
+        public string PostCode { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime JoinDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
