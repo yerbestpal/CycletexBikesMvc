e7baac86a120a7b8a1f597e7b89114119a0c7091
Ross McLean <rmclean92@protonmail.com>
2020-05-04 13:15:13 +0100

    Update ApplicationUser class
    - Add properties, annotations and XML comments

--- CycletexBikesMvc/Models/ApplicationUser.cs
+++ CycletexBikesMvc/Models/ApplicationUser.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -7,8 +9,16 @@ using Microsoft.AspNet.Identity.EntityFramework;
 namespace CycletexBikesMvc.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+
+    /// <summary>
+    /// The main ApplicationUser class.
+    /// Contains properties needed for Staff and Customer classes, including those derived from IdentityUser class.
+    /// </summary>
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        public string Name { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
