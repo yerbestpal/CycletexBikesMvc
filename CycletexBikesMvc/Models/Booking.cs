117779d8b9da623fcfeedefd0507b2a7494cc546
Ross McLean <rmclean92@protonmail.com>
2020-05-06 00:34:43 +0100

    Install DayPilot. Create Booking class. Implement payementsTypes.

---
+++ CycletexBikesMvc/Models/Booking.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CycletexBikesMvc.Models
+{
+    /// <summary>
+    /// Booking class.
+    /// </summary>
+    public class Booking
+    {
+        public DateTime Date { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public string CardNumber { get; set; }
+        public DateTime CardExpiryDate { get; set; }
+        public int CardSecurityNo { get; set; }
+        public int Total { get; set; }
+
+        // Navigational properties
+
+        [ForeignKey("Category")]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        // Job MANY prop here
+
+    }
+}
---
+++ CycletexBikesMvc/Models/PaymentType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CycletexBikesMvc.Models
+{
+    public class PaymentType
+    {
+        public PaymentTypes Type { get; set; }
+    }
+
+    public enum PaymentTypes
+    {
+        DebitCard,
+        PayPal
+    }
+
+    // Insert nav properties.
+}
