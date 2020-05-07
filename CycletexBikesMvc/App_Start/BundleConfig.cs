f8a2bafb0863f333d706070ae8f64587e74be05e
Ross McLean <rmclean92@protonmail.com>
2020-05-07 14:21:54 +0100

    Add DayPilot script to bundle

--- CycletexBikesMvc/App_Start/BundleConfig.cs
+++ CycletexBikesMvc/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace CycletexBikesMvc
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/DayPilot/daypilot-all.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
