07a6dd14d5669c403c2f166a5509fbdc840187dc
Ross McLean <rmclean92@protonmail.com>
2020-05-16 17:03:39 +0100

    Create SMS Send view.

--- CycletexBikesMvc/Controllers/SMSController.cs
+++ CycletexBikesMvc/Controllers/SMSController.cs
@@ -3,87 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Nexmo.Api;
 
 namespace CycletexBikesMvc.Controllers
 {
     public class SMSController : Controller
     {
-        // GET: SMS
-        public ActionResult Index()
+        [HttpGet]
+       public ActionResult Send()
         {
             return View();
-        }
-
-        // GET: SMS/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: SMS/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: SMS/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: SMS/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: SMS/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: SMS/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: SMS/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
