87b232b23cedebe7ce02812ab17b0f723a95fe22
Ross McLean <rmclean92@protonmail.com>
2020-05-16 17:07:59 +0100

    Add SMS Send POST action.

--- CycletexBikesMvc/Controllers/SMSController.cs
+++ CycletexBikesMvc/Controllers/SMSController.cs
@@ -10,8 +10,20 @@ namespace CycletexBikesMvc.Controllers
     public class SMSController : Controller
     {
         [HttpGet]
-       public ActionResult Send()
+        public ActionResult Send()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Send(string to, string text)
+        {
+            var results = SMS.Send(new SMS.SMSRequest
+            {
+                from = Configuration.Instance.Settings["appsettings:NEXMO_FROM_NUMBER"],
+                to = to,
+                text = text
+            });
             return View();
         }
     }
